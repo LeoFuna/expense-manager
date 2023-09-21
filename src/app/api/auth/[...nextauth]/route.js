2ce2ae9e880efe90d70346b5ba44c84a9d23e0b2
leofuna <leo.funa@gmail.com>
2023-09-20 22:10:43 -0300

    fix: removing ts on auth api route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,13 @@
-import NextAuth, { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 const ONE_DAY = 24 * 60 * 60;
 
-export const authOptions: AuthOptions = {
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   jwt: {
@@ -44,7 +44,7 @@ export const authOptions: AuthOptions = {
   //     return '/signin';
   //   },
   // },
-  secret: process.env.JWT_SECRET as string,
+  secret: process.env.JWT_SECRET,
 };
 
 const handler = NextAuth(authOptions);
