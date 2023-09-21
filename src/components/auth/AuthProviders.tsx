1c001e13538bc568702f7cba005394a4d7a1892f
leofuna <leo.funa@gmail.com>
2023-09-21 18:35:43 -0300

    refactor: making Auth providers component client side

--- src/components/auth/AuthProviders.tsx
+++ src/components/auth/AuthProviders.tsx
@@ -1,9 +1,16 @@
+'use client'
 import { getProviders } from 'next-auth/react';
 import { LoginButton } from './LoginButton';
+import { useEffect, useState } from 'react';
 
-export default async function AuthProviders() {
-  const providers = await getProviders();
-  const oAuthProviders = Object.values(providers ?? []);
+export default function AuthProviders() {
+  const [oAuthProviders, setOAuthProviders] = useState<any[]>([]);
+  useEffect(() => {
+    getProviders().then((providers) => {
+      setOAuthProviders(Object.values(providers ?? []));
+
+    });
+  }, [])
 
   return oAuthProviders.map((provider) => (
     <LoginButton key={provider.id} provider={provider} />
