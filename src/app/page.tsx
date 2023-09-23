eab70708f37711826908b14db14fa34322ce5885
leofuna <leo.funa@gmail.com>
2023-09-23 09:39:47 -0300

    fix: some debugging on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { getServerSession } from "next-auth"
+
+export default async function Home() {
+  const session = await getServerSession();
+  const account = await fetch(`${process.env.APP_URL}/api/accounts/meu@email.com`).then((data) => data.json());
+
   return (
     <main>
-      <p className="title-x">Home</p>
+      <p className="title-x">Olá {session?.user?.name}</p>
+      <p>{JSON.stringify(account)}</p>
     </main>
   )
 }
