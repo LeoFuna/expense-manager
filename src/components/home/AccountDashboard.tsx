423145ec523e4f72c6bdd59b09e7c3c3e9ca9bcc
leofuna <leo.funa@gmail.com>
2023-09-26 17:26:06 -0300

    feat: recents transactions on home

--- src/components/home/AccountDashboard.tsx
+++ src/components/home/AccountDashboard.tsx
@@ -30,6 +30,84 @@ export default function AccountDashboard() {
         </div>
       </div>
 
+      <div className='flex flex-col w-screen px-4 gap-2'>
+        <section className="flex justify-between items-center">
+          <h1 className='title-3 text-dark-75'>Transações Recentes</h1>
+          <button className='bg-primary-20 text-primary-100 px-4 py-2 rounded-xl'>Ver Todas</button>
+        </section>
+        <section className='flex flex-col items-between gap-7 py-4'>
+          <div className='flex justify-between'>
+            <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
+              <FiTrendingDown size={24} />
+             </div>
+            <div className='flex flex-col justify-between w-2/4'>
+              <h1 className='regular-l'>Shopping</h1>
+              <h3 className='regular-xs text-light-20'>Comprar uma roupa</h3>
+            </div>
+            <div className='flex flex-col justify-between items-end'>
+              <h1 className='title-3 text-danger-100'>-R$ 30,00</h1>
+              <h3 className='regular-xs text-light-20'>10:00 AM</h3>
+            </div>
+          </div>
+
+          <div className='flex justify-between'>
+            <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
+              <FiTrendingDown size={24} />
+             </div>
+            <div className='flex flex-col justify-between w-2/4'>
+              <h1 className='regular-l'>Freelancer</h1>
+              <h3 className='regular-xs text-light-20'>Website</h3>
+            </div>
+            <div className='flex flex-col justify-between items-end'>
+              <h1 className='title-3 text-success-100'>R$ 50,00</h1>
+              <h3 className='regular-xs text-light-20'>12:00 AM</h3>
+            </div>
+          </div>
+
+          <div className='flex justify-between'>
+            <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
+              <FiTrendingDown size={24} />
+             </div>
+            <div className='flex flex-col justify-between w-2/4'>
+              <h1 className='regular-l'>Padaria</h1>
+              <h3 className='regular-xs text-light-20'>Pao de batata</h3>
+            </div>
+            <div className='flex flex-col justify-between items-end'>
+              <h1 className='title-3 text-danger-100'>-R$ 10,00</h1>
+              <h3 className='regular-xs text-light-20'>15:00 AM</h3>
+            </div>
+          </div>
+
+          <div className='flex justify-between'>
+            <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
+              <FiTrendingDown size={24} />
+             </div>
+            <div className='flex flex-col justify-between w-2/4'>
+              <h1 className='regular-l'>Padaria</h1>
+              <h3 className='regular-xs text-light-20'>Pao de batata</h3>
+            </div>
+            <div className='flex flex-col justify-between items-end'>
+              <h1 className='title-3 text-danger-100'>-R$ 10,00</h1>
+              <h3 className='regular-xs text-light-20'>15:00 AM</h3>
+            </div>
+          </div>
+
+          <div className='flex justify-between'>
+            <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
+              <FiTrendingDown size={24} />
+             </div>
+            <div className='flex flex-col justify-between w-2/4'>
+              <h1 className='regular-l'>Padaria</h1>
+              <h3 className='regular-xs text-light-20'>Pao de batata</h3>
+            </div>
+            <div className='flex flex-col justify-between items-end'>
+              <h1 className='title-3 text-danger-100'>-R$ 10,00</h1>
+              <h3 className='regular-xs text-light-20'>15:00 AM</h3>
+            </div>
+          </div>
+        </section>
+      </div>
+
     </section>
   )
 }
