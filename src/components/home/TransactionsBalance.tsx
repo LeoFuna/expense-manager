a3e4d3f3a14ff6444adf39f8d0621b424d385b5a
leofuna <leo.funa@gmail.com>
2023-09-27 17:54:56 -0300

    fix: removing dinamy css tailwind class in correct way

--- src/components/home/TransactionsBalance.tsx
+++ src/components/home/TransactionsBalance.tsx
@@ -10,20 +10,20 @@ type TransactionsBalanceProps = {
 const getComponentColor = (color: TransactionsBalanceProps['color']) => {
   switch (color) {
     case 'red':
-      return 'danger';
+      return 'bg-danger-100 text-danger-100';
     case 'green':
-      return 'success';
+      return 'bg-success-100 text-success-100';
     case 'yellow':
-      return 'warning';
+      return 'bg-warning-100 text-warning-100';
     default:
-      return 'success';
+      return 'bg-success-100 text-success-100';
   }
 }
 
 export default function TransactionsBalance({ title, totalAmount, Icon, color }: TransactionsBalanceProps) {
   return (
-    <div className={`flex items-center bg-${getComponentColor(color)}-100 w-2/4 p-4 rounded-3xl gap-2`}>
-      <div className={`bg-light-100 flex items-center w-12 h-12 p-2 rounded-xl text-${getComponentColor(color)}-100`}>
+    <div className={`flex items-center ${getComponentColor(color)} w-2/4 p-4 rounded-3xl gap-2`}>
+      <div className={`bg-light-100 flex items-center w-12 h-12 p-2 rounded-xl`}>
         <Icon size={24} />
       </div>
       <div className='text-light-100 flex flex-col items-start gap-2 w-3/4'>
