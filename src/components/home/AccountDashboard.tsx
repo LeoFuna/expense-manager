9b9951d2be1c1f6cc46c33810d9e8546cf8a7358
leofuna <leo.funa@gmail.com>
2023-09-27 16:00:24 -0300

    style: more css work on scroll for account dashbord

--- src/components/home/AccountDashboard.tsx
+++ src/components/home/AccountDashboard.tsx
@@ -2,7 +2,7 @@ import { FiTrendingDown, FiTrendingUp } from 'react-icons/fi';
 
 export default function AccountDashboard() {
   return (
-    <section className="px-4 py-4 flex flex-col items-center gap-7 w-screen">
+    <section className="overflow-hidden px-4 py-4 pb-14 flex flex-col items-center gap-7 w-screen">
       <div className="flex flex-col items-center gap-2">
         <h2 className="regular-s text-light-20">Saldo da Conta</h2>
         <h3 className="title-1 text-dark-75">R$ 1.000,00</h3>
@@ -15,7 +15,7 @@ export default function AccountDashboard() {
           </div>
           <div className='text-light-100 flex flex-col items-start gap-2 w-3/4'>
             <h4 className='regular-s'>Entradas</h4>
-            <p className='title-2 text-base'>R$ 10.000</p>
+            <p className='title-2 text-sm'>R$ 10.000</p>
           </div>
         </div>
 
@@ -25,7 +25,7 @@ export default function AccountDashboard() {
           </div>
           <div className='text-light-100 flex flex-col items-start gap-2 w-3/4'>
             <h4 className='regular-s'>Saídas</h4>
-            <p className='title-2 text-base'>R$ 10.000</p>
+            <p className='title-2 text-sm'>R$ 10.000</p>
           </div>
         </div>
       </div>
@@ -35,7 +35,7 @@ export default function AccountDashboard() {
           <h1 className='title-3 text-dark-75'>Transações Recentes</h1>
           <button className='bg-primary-20 text-primary-100 px-4 py-2 rounded-xl'>Ver Todas</button>
         </section>
-        <section className='flex flex-col items-between gap-7 py-4'>
+        <section className='flex flex-col items-between gap-7 h-[42vh] py-4 overflow-y-scroll scrollbar-hidden'>
           <div className='flex justify-between'>
             <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
               <FiTrendingDown size={24} />
@@ -61,6 +61,20 @@ export default function AccountDashboard() {
             <div className='flex flex-col justify-between items-end'>
               <h1 className='title-3 text-success-100'>R$ 50,00</h1>
               <h3 className='regular-xs text-light-20'>12:00 AM</h3>
+            </div>
+          </div>
+
+          <div className='flex justify-between'>
+            <div className="bg-primary-20 flex items-center justify-center w-12 h-12 rounded-xl text-primary-100">
+              <FiTrendingDown size={24} />
+             </div>
+            <div className='flex flex-col justify-between w-2/4'>
+              <h1 className='regular-l'>Padaria</h1>
+              <h3 className='regular-xs text-light-20'>Pao de batata</h3>
+            </div>
+            <div className='flex flex-col justify-between items-end'>
+              <h1 className='title-3 text-danger-100'>-R$ 10,00</h1>
+              <h3 className='regular-xs text-light-20'>15:00 AM</h3>
             </div>
           </div>
 
