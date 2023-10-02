0b778ba47df6618380be407968be7b9b9e0bcd46
leofuna <leo.funa@gmail.com>
2023-10-02 16:19:03 -0300

    feat: account interfaces fot new users

--- src/interfaces/repositories/AccountRepository.ts
+++ src/interfaces/repositories/AccountRepository.ts
@@ -1,6 +1,7 @@
 import { IAccount } from "../Account";
 
 export default interface IAccountRepository {
-  show(email: string): Promise<IAccount | null>;
+  show(email: string, year: number, month: number): Promise<IAccount | null>;
   create(data: IAccount): Promise<IAccount>;
+  verifyIfAccountExists(email: string): Promise<boolean>;
 }
--- src/interfaces/services/AccountService.ts
+++ src/interfaces/services/AccountService.ts
@@ -1,6 +1,7 @@
 import { IAccount } from "../Account";
 
 export interface IAccountService {
-  show(email: string): Promise<IAccount>;
+  show(email: string, year: number, month: number): Promise<IAccount | null>;
   create(data: IAccount): Promise<IAccount>;
+  checkIfIsANewUser(email: string): Promise<boolean>;
 }
