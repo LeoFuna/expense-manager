0c5f79eeacdc6b5f2ba231c512e1ea2ad5dd3cbf
leofuna <leo.funa@gmail.com>
2023-10-02 16:18:28 -0300

    fix: controller account for new user accounts

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -8,15 +8,16 @@ export default class AccountController implements IAccountController {
 
   async create(req: NextRequest, res: NextResponse<unknown>): Promise<NextResponse<IAccount>> {
     const body: IAccount = await req.json();
+
     const serviceResponse = await this.accountService.create(body);
 
     return NextResponse.json(serviceResponse, { status: 201 });
   }
 
-  async show(req: NextRequest, { params }: { params: { email: string } }): Promise<NextResponse<IAccount>> {
-    const { email } = params;
+  async show(req: NextRequest, { params }: { params: { email: string, year: number, month: number } }): Promise<NextResponse<IAccount | null>> {
+    const { email, year, month } = params;
 
-    const serviceResponse = await this.accountService.show(email);
+    const serviceResponse = await this.accountService.show(email, year, month);
 
     return NextResponse.json(serviceResponse, { status: 200 });
   }  
