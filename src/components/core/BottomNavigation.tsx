0ee7a111527a56c6382eccee3dac177e2afc187b
leofuna <leo.funa@gmail.com>
2023-10-02 17:12:31 -0300

    feat: bottom navigation component changing colors idea

--- src/components/core/BottomNavigation.tsx
+++ src/components/core/BottomNavigation.tsx
@@ -1,19 +1,25 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { FaHouse, FaPlus } from 'react-icons/fa6';
+
+
 export default function BottomNavigation() {
   const router = useRouter();
+  const pathName = usePathname()
+  const setButtonsColor = (buttonPathName: string) => (
+    pathName === buttonPathName ? 'text-primary-100' : 'text-light-20'
+  )
   return (
     <nav className='fixed bottom-0 w-screen h-20 bg-light-80 flex justify-around items-center'>
       <button
-        className='text-primary-100 flex flex-col justify-center items-center gap-1'
+        className={`${setButtonsColor('/')} flex flex-col justify-center items-center gap-1`}
         onClick={() => router.push('/')}
       >
         <FaHouse size={24} />
         <h1 className='regular-xs'>Home</h1>
       </button>
       <button
-        className='text-light-20 flex flex-col justify-center items-center gap-1'
+        className={`${setButtonsColor('/transactions')} text-light-20 flex flex-col justify-center items-center gap-1`}
         onClick={() => router.push('/transactions')}
       >
         <FaHouse size={24} />
