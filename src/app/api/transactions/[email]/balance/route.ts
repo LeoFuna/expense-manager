f3eab5ba2c1c5c32f410995a5f2ccb1e1cd9c83f
leofuna <leo.funa@gmail.com>
2023-10-09 18:17:44 -0300

    fix: update transactions controller instance

--- src/app/api/transactions/[email]/balance/route.ts
+++ src/app/api/transactions/[email]/balance/route.ts
@@ -1,9 +1,11 @@
 import TransactionController from "@/controllers/TransactionController";
+import AccountRepoFirebase from "@/repositories/AccountRepoFirebase";
 import TransactionRepoFirebase from "@/repositories/TransactionRepoFirebase";
+import AccountService from "@/services/AccountService";
 import TransactionService from "@/services/TransactionService";
 import { NextRequest } from "next/server";
 
-const transactionController = new TransactionController(new TransactionService(new TransactionRepoFirebase()));
+const transactionController = new TransactionController(new TransactionService(new TransactionRepoFirebase(), new AccountService(new AccountRepoFirebase())));
 
 export async function GET(request: NextRequest, { params }: { params: { email: string } }) {
   return transactionController.getBalance(request, { params });
