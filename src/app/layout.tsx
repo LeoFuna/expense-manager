a464bb266cdefb801c07abf955b02ff9b436b59f
leofuna <leo.funa@gmail.com>
2023-10-09 15:55:38 -0300

    debug: tests remove cliente session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
-        <ClientSessionProvider session={session}>
+        {/* <ClientSessionProvider session={session}> */}
           {children}
-        </ClientSessionProvider>
+        {/* </ClientSessionProvider> */}
       </body>
     </html>
   )
