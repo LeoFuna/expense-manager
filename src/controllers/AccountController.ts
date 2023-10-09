bf2c3decb4418fe455e6389b7656feb64c135cc2
leofuna <leo.funa@gmail.com>
2023-10-09 18:20:25 -0300

    feat: implement updater account controller

--- src/controllers/AccountController.ts
+++ src/controllers/AccountController.ts
@@ -5,6 +5,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default class AccountController implements IAccountController {
   constructor(private readonly accountService: IAccountService) {}
+  async update(req: NextRequest, { params }: { params: { email: string; }; }): Promise<NextResponse<{ id: string; } | null>> {
+    const { searchParams } = new URL(req.url);
+    const month = searchParams.get('month');
+    const fullYear = searchParams.get('fullYear');
+    const { email } = params;
+    const body: IAccount = await req.json();
+
+    if (!fullYear || !month) {
+      return NextResponse.json(null, { status: 400 });
+    }
+    const serviceResponse = await this.accountService.update(email, body,  Number(fullYear), Number(month));
+
+    return NextResponse.json(serviceResponse, { status: 204 });
+  }
 
   async create(req: NextRequest, res: NextResponse<unknown>): Promise<NextResponse<IAccount>> {
     const body: IAccount = await req.json();
