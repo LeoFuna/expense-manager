01b824f41756eae26fff720de5fb51d9f532836a
leofuna <leo.funa@gmail.com>
2023-10-19 11:07:50 -0300

    feat: using new components on new transactions page

--- src/app/transactions/new/page.tsx
+++ src/app/transactions/new/page.tsx
@@ -1,23 +1,33 @@
-'use client'
 import Navigation from "@/components/transactions/new/Navigation";
+import TransactionAmount from "@/components/transactions/new/TransactionAmount";
+import TransactionDetails from "@/components/transactions/new/TransactionDetails";
+import { DateContextProvider } from "@/contexts/dateContext";
 
-const getBgColor = (type?: string) => {
+const getBgColor = (type: string) => {
   if (type === 'income') return 'bg-success-100';
   return 'bg-danger-100';
 }
-const getNavTitle = (type?: string) => {
+const getNavTitle = (type: string) => {
   if (type === 'income') return 'Nova Entrada';
   return 'Nova Saída';
 }
 
-export default function NewTransaction({ searchParams }: { searchParams: { type?: string } }) {  
+export default function NewTransaction({ searchParams }: { searchParams: { type: 'income' | 'outcome' } }) {  
   return (
     <main className="flex flex-col items-center h-screen">
-      <div
-        className={`w-screen max-w-md h-full ${getBgColor(searchParams?.type)}`}
-      >
-        <Navigation title={getNavTitle(searchParams?.type)} />
-      </div>
+      <DateContextProvider>
+        <div
+          className={`
+            w-screen relative max-w-md h-full
+            ${getBgColor(searchParams?.type)}
+            border-x border-dark-25
+          `}
+        >
+          <Navigation title={getNavTitle(searchParams?.type)} />
+          <TransactionAmount type={searchParams.type} />
+          <TransactionDetails />
+        </div>
+      </DateContextProvider>
     </main>
   )
 }
---
+++ src/components/transactions/new/TransactionDetails.tsx
@@ -0,0 +1,16 @@
+/*
+  - Categoria: Terao os itens de transactionCategory
+    lembrando de considerar o operationType para listar somente os tipos certos
+  - Descrição: campo simples de texto
+  - Anexo: TO DO: inicialmente nao tera mas planejaremos
+  - Recorrente: TO DO: planejar como implementar para que nao precisemos todos
+    os meses criar transacoes de custos fixos
+  - Botao para processeguir
+*/
+export default function TransactionDetails() {
+  return (
+    <div className="absolute h-[65%] rounded-t-3xl w-full bottom-0 left-0 right-0 bg-light-100">
+
+    </div>
+  )
+}
