3f286fc7fa570b72c181024b807573270d47e068
leofuna <leo.funa@gmail.com>
2023-10-23 12:26:09 -0300

    feat: all api env for transaction categories index

--- src/controllers/TransactionCategoryController.ts
+++ src/controllers/TransactionCategoryController.ts
@@ -5,6 +5,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default class TransactionCategoryController implements ITransactionCategoryController {
   constructor(private readonly transactionCategoryService: ITransactionCategoryService) {}
+  async index(request: NextRequest): Promise<NextResponse<ITransactionCategory[]>> {
+    const { searchParams } = new URL(request.url);
+    const operationType = searchParams.get('operationType');
+
+    const categories = await this.transactionCategoryService.index(operationType as ITransactionCategory['operationType']);
+
+    return NextResponse.json(categories, { status: 200 })
+  }
 
   async show(request: NextRequest, { params }: { params: { id: string; }; }): Promise<NextResponse<ITransactionCategory | null>> {
     const category = await this.transactionCategoryService.show(params.id);
--- src/services/TransactionCategoryService.ts
+++ src/services/TransactionCategoryService.ts
@@ -4,6 +4,11 @@ import { ITransactionCategoryService } from "@/interfaces/services/TransactionCa
 
 export default class TransactionCategoryService implements ITransactionCategoryService {
   constructor(private readonly transactionCategoryRepo: ITransactionCategoryRepo) {}
+  async index(operationType: ITransactionCategory['operationType']): Promise<ITransactionCategory[]> {
+    const categories = await this.transactionCategoryRepo.index(operationType);
+
+    return categories;
+  }
 
   async show(id: string): Promise<ITransactionCategory | null> {
     const category = await this.transactionCategoryRepo.show(id);
