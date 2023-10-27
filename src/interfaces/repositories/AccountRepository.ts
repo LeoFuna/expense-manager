0dcde5ea3f9add581f4a6a9a00fef181e6ccfac4
leofuna <leo.funa@gmail.com>
2023-10-27 18:05:46 -0300

    feat: account repo index

--- src/interfaces/repositories/AccountRepository.ts
+++ src/interfaces/repositories/AccountRepository.ts
@@ -1,6 +1,7 @@
 import { IAccount, IAccountApi } from "../Account";
 
 export default interface IAccountRepository {
+  index(): Promise<{ email: string }[]>;
   show(email: string, year: number, month: number): Promise<IAccountApi | null>;
   create(data: IAccount): Promise<IAccount>;
   update(email: string, data: {id: string, balanceInCents: number}, year: number): Promise<{id: string}>;
