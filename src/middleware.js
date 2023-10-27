f1e64828634774db0e0d8296dbe7adb6051234e7
leofuna <leo.funa@gmail.com>
2023-10-27 18:06:07 -0300

    fix: some protected pages

--- src/middleware.js
+++ src/middleware.js
@@ -3,4 +3,8 @@ export { default } from "next-auth/middleware"
 /**
  * @matcher {string[]} - Array of pages that you want to protect
  */
-export const config = { matcher: ["/"] }
+export const config = { matcher: [
+  "/",
+  "/transactions",
+  "/transactions/new"
+]}
