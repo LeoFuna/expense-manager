f5aa44ae3c1cfddf3efbee904304a85b992d2566
leofuna <leo.funa@gmail.com>
2023-11-09 13:54:27 -0300

    fix: change GET to POST on monthlyAcc

--- src/app/api/accounts/monthlyCreation/route.ts
+++ src/app/api/accounts/monthlyCreation/route.ts
@@ -3,6 +3,6 @@ import { NextRequest } from "next/server";
 
 const accountController = new AccountControllerFactory().createAccountController();
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   return accountController.createMonthAccounts(request);
 }
