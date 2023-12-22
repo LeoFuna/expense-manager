53aa1ab0078ad94d35a3f92adb335b993de19179
leofuna <leo.funa@gmail.com>
2023-12-22 16:04:49 -0300

    feat: budget page skeleton

---
+++ src/app/budget/page.tsx
@@ -0,0 +1,18 @@
+import BottomNavigation from "@/components/core/BottomNavigation";
+import TopNavigation from "@/components/core/TopNavigation";
+import UnderDevelopment from "@/components/core/UnderDevelopment";
+import { getServerSession } from "next-auth";
+
+export default async function Budget() {
+  const session = await getServerSession();
+  return (
+    <main className="flex flex-col items-center h-[100dvh]">
+      <TopNavigation
+        avatar={session?.user?.image}
+        hideNavigation
+      />
+      <UnderDevelopment />
+      <BottomNavigation />
+    </main>
+  )
+}
