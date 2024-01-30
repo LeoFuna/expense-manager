5defd710c862041b0c7f00398823480fbe1b93ed
leofuna <leo.funa@gmail.com>
2024-01-30 19:09:35 -0300

    chore: jest configs to use env in tests

--- jest.config.ts
+++ jest.config.ts
@@ -18,9 +18,9 @@ const config: Config = {
   },
   transformIgnorePatterns: [
     '/node_modules/'
-  ]
+  ],
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
